27f16c009bf7907217ca764fcacd3d388d24b086
ColePhilips <98480898+ElocCode@users.noreply.github.com>
2025-10-28 16:47:22 -0400

    completed part 2 of todo list project and finished lesson on arrays and loops

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,7 @@
-let todoList = [];
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+}];
 
 renderTodoList();
 
@@ -7,8 +10,18 @@ function renderTodoList(){
     let todoListHTML = '';
 
     for(let i = 0; i < todoList.length; i++){
-        const todo = todoList[i];
-        const html = `<p>${todo}</p>`;
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const {name, dueDate} = todoObject;
+        const html = `
+        <div>${name}</div> 
+        <div>${dueDate}</div>
+        <button onclick = "
+            todoList.splice(${i}, 1);
+            renderTodoList();
+        " class="delete-todo-button">Delete</button>
+        `;
         todoListHTML += html;
     }
 
@@ -21,10 +34,17 @@ function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
 
-    todoList.push(name);
+    const dateInputElement = document.querySelector('.js-due-date-input')
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        name, 
+        dueDate
+    });
     console.log(todoList);
 
     inputElement.value = '';
+    dateInputElement.value = '';
 
     renderTodoList();
 }
